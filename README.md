d5bed88a05f0b30e737c4b0125674f38e978cd73
imtiyazalikhan <65025085+imtiyazalikhan@users.noreply.github.com>
2021-06-09 20:01:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c-86
+# book-santa-stage-3
+Stage -3
---
+++ components/AppDrawerNavigator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import { AppTabNavigator } from './AppTabNavigator'
+import CustomSideBarMenu  from './CustomSideBarMenu';
+import  BookDonateScreen from '../screens/BookDonateScreen'
+import NotificationScreen from '../screens/NotificationScreen';
+import SettingScreen from '../screens/SettingScreen';
+
+export const AppDrawerNavigator = createDrawerNavigator({
+  Home : {
+    screen : AppTabNavigator
+    },
+  BookDonate : {
+    screen : BookDonateScreen
+  },
+  Notification : {
+    screen : NotificationScreen
+  },
+  Setting : {
+    screen : SettingScreen
+  }
+},
+  {
+    contentComponent:CustomSideBarMenu
+  },
+  {
+    initialRouteName : 'Home'
+  })
